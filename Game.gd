77797e9a65d5f5dde9ed7474195a11ac6631e48e
hrk4649 <58840595+hrk4649@users.noreply.github.com>
2022-02-23 16:13:06 +0900

    copy  Viewport image into TextureRect

--- Game.gd
+++ Game.gd
@@ -3,16 +3,19 @@ extends Node2D
 
 var scene_instance = null
 
+var num_pieces = 9
+
+var piece_size = Vector2(100,100)
+
+var top_left_position = Vector2(100,100)
+
+var pieces
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
     # load scene and add to the viewport
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
-
+    top_left_position = $PieceTopLeft.position
 
 
 func _process(delta):
@@ -20,24 +23,54 @@ func _process(delta):
     if(!visible):
         return
     pass
-#    var img = $Viewport.get_texture().get_data()
-#    img.flip_y()
-#    # Convert Image to ImageTexture.
-#    var tex = ImageTexture.new()
-#    tex.create_from_image(img)
-#
-#    var atlas_tex = AtlasTexture.new()
-#    atlas_tex.atlas = tex
-#    atlas_tex.region = Rect2(Vector2(0, 0), Vector2(400,400))
-#    $TextureRect.texture = atlas_tex
-    $TextureRect.texture = $Viewport.get_texture()
+    change_texture_rect(delta)
 
+func change_texture_rect(delta):
+    var tex = $Viewport.get_texture()
+    for i in range(0, num_pieces):
+        var tr = pieces[i]
+        var atlas_tex = AtlasTexture.new()
+        atlas_tex.atlas = tex
+        var position = get_picture_position(i)
+        atlas_tex.region = Rect2(position, piece_size)
+        tr.texture = atlas_tex
+    
+func get_initial_position(idx = 0):
+    var num = int(sqrt(num_pieces))
+    var x = (idx % num) * piece_size.x * 1.05
+    var y = int(idx / num) * piece_size.y * 1.05
+    return Vector2(x,y)
+
+func get_picture_position(idx = 0):
+    var num = int(sqrt(num_pieces))
+    var x = (idx % num) * piece_size.x
+    var y = int(idx / num) * piece_size.y
+    return Vector2(x,y)
 
 func init_game(level, num_pieces):
     pass
-    print("(level,num_pices) = (" + str(level) + "," + str(num_pieces))
+    print("(level,num_pices) = (" + str(level) + "," + str(num_pieces) + ")")
+
+    self.num_pieces = num_pieces
     scene_instance = load("res://levels/dragon_fly/Main.tscn").instance()
     $Viewport.add_child(scene_instance)
+    init_pieces(num_pieces)
+
+func init_pieces(num_pieces):    
+    # prepare pieces TextureRect
+    pieces = []
+    for i in range(0, num_pieces):
+        var tr = TextureRect.new()
+        var position = get_initial_position(i) + top_left_position
+        tr.set_position(position)
+        tr.set_size(piece_size)
+
+        #tr.hint_tooltip = str(i)
+        
+        pieces.push_back(tr)
+        var logstr = "position:" + str(tr.rect_global_position) + " visible:" + str(tr.visible) 
+        print(logstr)
+        add_child(tr)
 
 func stop_game():
     """
@@ -47,6 +80,9 @@ func stop_game():
     print("stop_game()")
     $Viewport.remove_child(scene_instance)
     scene_instance = null
+    for tr in pieces:
+        remove_child(tr)
+    pieces = null
 
 func _on_ButtonExit_button_up():
     pass # Replace with function body.
