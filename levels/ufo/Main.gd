e34a234023eb5a30bb14f3b0b52e0dbb6ed71151
hrk4649 <58840595+hrk4649@users.noreply.github.com>
2022-04-15 06:51:42 +0900

    add UFO to level

--- levels/ufo/Main.gd
+++ levels/ufo/Main.gd
@@ -1,28 +1,16 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
     play_ufo_animation()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
 func play_ufo_animation():
     var list = $AnimationPlayer.get_animation_list()
-    print("list:"+str(list))
+    #print("list:"+str(list))
     # skip choosing list[0] because it is "RESET" 
     var idx = randi() % (list.size() - 1) + 1
-    print("idx:"+str(idx))
+    #print("idx:"+str(idx))
     var anim = list[idx]
     $AnimationPlayer.play(anim)
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-    pass # Replace with function body.
     play_ufo_animation()
