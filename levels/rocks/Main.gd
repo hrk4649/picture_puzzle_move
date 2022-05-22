5475ee5d8c22df923833a592ce29fd73b2692aa5
hrk4649 <58840595+hrk4649@users.noreply.github.com>
2022-05-23 08:25:00 +0900

    #1 remove comments

--- levels/rocks/Main.gd
+++ levels/rocks/Main.gd
@@ -1,22 +1,10 @@
 extends Spatial
 
-
 var Rock = load(get_script().resource_path.get_base_dir() + "/Rock.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
     $Timer.start()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
-
 
 func _on_Timer_timeout():
     pass # Replace with function body.
