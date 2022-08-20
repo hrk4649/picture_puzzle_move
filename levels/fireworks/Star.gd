f17679fed35ffe6bf95aa16c096b4d943a5d2ce2
hrk4649 <58840595+hrk4649@users.noreply.github.com>
2022-08-20 13:18:00 +0900

    improve fireworks

--- levels/fireworks/Star.gd
+++ levels/fireworks/Star.gd
@@ -1,32 +1,30 @@
 extends Spatial
 
+export var duration:float = 0
+export var max_deviation:float = 1.0
 
-var velocity = Vector3.UP * 50
+var burn_time = 0
 
-var gravity = Vector3(0, -9.8, 0)
+var deviation = 0
 
-# Called when the node enters the scene tree for the first time.
+# velocity is initialized by Ball.gd
+var velocity = Vector3.UP
+
+var gravity = Vector3.DOWN * 9.8 * 0.5
+
 func _ready():
     pass # Replace with function body.
+    deviation = rand_range(0.0, max_deviation)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     if visible:
-        var slow = 1.0
-        transform.origin += velocity * delta * slow
-        velocity += gravity * delta * slow
+        transform.origin += velocity * delta
+        var friction = velocity * - 0.9
+        velocity += (gravity + friction) * delta
 
-        # remove the node if origin of this node is out of positions
-        var pos1 = $Positions/Position3D1.transform.origin
-        var pos2 = $Positions/Position3D2.transform.origin
-        var this = transform.origin
-        
-        var out_of_positions = (
-            this.x < pos1.x || this.x > pos2.x
-            || this.y < pos1.y || this.y > pos2.y
-            || this.z < pos1.z || this.z > pos2.z
-        )
-        
-        if out_of_positions:
-            #print("star: visible = false")
+        burn_time += delta
+
+        if burn_time > duration + deviation:
             visible = false
+            burn_time = 0
+            deviation = rand_range(0.0, max_deviation)
