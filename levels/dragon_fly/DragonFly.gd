46ada0eb659988e08869b02c481c4e08d133ae62
hrk4649 <58840595+hrk4649@users.noreply.github.com>
2022-08-20 11:18:31 +0900

    use tween in dragon_fly level

--- levels/dragon_fly/DragonFly.gd
+++ levels/dragon_fly/DragonFly.gd
@@ -1,37 +1,51 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-var speed = 300
-
 var velocity = Vector2(0, 0)
 
-# Called when the node enters the scene tree for the first time.
+var tween_phase = 0
+
 func _ready():
-    pass # Replace with function body.
-    #var radian = velocity.angle()
-    #rotation = PI * 0.5 + velocity.angle()
-
-func set_velocity_by_rotation():
     pass
-    var fix = rotation - PI * 0.5
-    velocity = Vector2(cos(fix), sin(fix)) * speed
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
 
 func _physics_process(delta):
     pass
-    var vel_delta = velocity * delta
-    global_transform.origin += vel_delta
+ 
+func set_velocity_by_rotation():
+    pass
+    var fix = rotation - deg2rad(90)
+    velocity = Vector2(cos(fix), sin(fix))
+    tween_move()
+
+func tween_move():
+    pass
+    var tween = $Tween
+    var destination = position + velocity * rand_range(60, 360)
+    tween.interpolate_property(self, "position", self.position, destination, 0.5,
+        Tween.TRANS_QUAD ,Tween.EASE_IN_OUT)
+    tween.start()
+
+func tween_hover():
+    pass
+    var tween = $Tween
+    var destination = position + velocity * rand_range(10, 60)
+    var duration = rand_range(0, 1.0)
+    tween.interpolate_property(self, "position", self.position, destination, duration,
+        Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+    tween.start()
 
 
 func _on_VisibilityNotifier2D_screen_exited():
-    pass # Replace with function body.
-    #print("_on_VisibilityNotifier2D_screen_exited")
+    pass
     queue_free()
+
+func _on_Tween_tween_all_completed():
+    pass
+    tween_phase = (tween_phase + 1) %2
+    match(tween_phase):
+        0:
+            tween_move()
+        1:
+            tween_hover()
+        _:    
+            print("unexpected:value = " + str(tween_phase))
+       
