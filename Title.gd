1d8d16ec725e0d29317a32264037e900422e012a
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2024-01-13 16:55:09 +0900

    Squashed commit of the following:
    commit fe83ed8a50178e330f03215c8579bb727fc7dd83
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Sat Jan 13 16:52:31 2024 +0900
    
        ver 0.2.1
    
    commit f6613b3be8ad03cf733859fe024e8d97adc66da8
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Sat Jan 13 15:31:08 2024 +0900
    
        change build version
    
    commit 8ba8303f8a39c1e392f1eafe52e16a0312e5b39c
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Sat Jan 13 14:48:31 2024 +0900
    
        add admob plugin for android
    
    commit 73d3feaeb788b3d2dda725562e5687dfdd92db99
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Sat Jan 13 08:43:19 2024 +0900
    
        adjust UIs
    
    commit 2ec8b221c90bcc3d6f32439404aacb8a3051e567
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Sat Jan 13 08:26:26 2024 +0900
    
        enable ad
    
    commit 25f30f413a43279ce9a68ebac20448a92e00f723
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Fri Jan 12 08:46:01 2024 +0900
    
        add CoinManager
    
    commit b9bef71efe7ce7d2695f9c29b9217dfc737404ab
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Wed Jan 10 08:37:55 2024 +0900
    
        success to show ad
    
    commit 2c39d64f12bb24da6f544d332d0728ce8947fd20
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Wed Jan 10 07:10:10 2024 +0900
    
        add godot-admob-plugin and godot-admob-ios
    
    commit 7e73031a72587d78e5dd3c9549d1aa95d4137128
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Tue Jan 9 07:12:40 2024 +0900
    
        add import_pvrtc=true for ios
    
    commit b6bd2766ac58f97a8b2f4181ccf387c7c62e8855
    Author: Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
    Date:   Tue Jan 9 07:12:10 2024 +0900
    
        add icons

--- Title.gd
+++ Title.gd
@@ -1,21 +1,74 @@
 extends Control
 
+onready var coin = $Coin
+onready var watch_ad = $HBoxContainer/WatchAd
+onready var get_coin = $HBoxContainer/GetCoin
+onready var start = $Start
+
+var coin_to_play = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
-    $AnimationPlayer.play("forth_and_back")
+	pass # Replace with function body.
+	$AnimationPlayer.play("forth_and_back")
+	AdsManager.connect("init_completed", self, "_on_ad_init_completed")
+	AdsManager.connect("ad_loaded", self, "_on_ad_loaded")
+	AdsManager.connect("earned_rewarded", self, "_on_earned_rewarded")
+	AdsManager.connect("ad_closed", self, "_on_ad_closed")
+
+func init_coin() -> void:
+	coin.text = "YOUR COIN: %s" % CoinManager.get_coin()
+	start.disabled = !CoinManager.has_coin(coin_to_play)
+
+func init_button() -> void:
+	watch_ad.disabled = !AdsManager.is_loaded()
+	get_coin.disabled = false
+
+func init_title():
+	print("init_title")
+	init_coin()
+	init_button()
+	if AdsManager.is_available() && !AdsManager.is_loaded():
+		AdsManager.load_ad()
 
 func grab_focus():
-    pass
-    $Start.grab_focus()
+	pass
+	$Start.grab_focus()
 
 func _on_Credit_pressed():
-    pass # Replace with function body.
-    $AudioStreamPlayer.play()
-    get_parent().change_scene_credit()
-
+	$AudioStreamPlayer.play()
+	get_parent().change_scene_credit()
 
 func _on_Start_pressed():
-    pass # Replace with function body.
-    $AudioStreamPlayer.play()
-    get_parent().change_scene_level()
+	if CoinManager.has_coin(coin_to_play):
+		CoinManager.use_coin(coin_to_play)
+		$AudioStreamPlayer.play()
+		get_parent().change_scene_level()
+
+func _on_ad_init_completed(status, adapter_name):
+	print("_on_ad_init_completed")
+	AdsManager.load_ad()
+
+func _on_ad_loaded():
+	print("_on_ad_loaded")
+	watch_ad.disabled = false
+
+func _on_earned_rewarded(currency, amount):
+	print("_on_earned_rewarded")
+	CoinManager.add_coin(amount)
+	#CoinManager.add_coin(1)
+
+func _on_ad_closed():
+	print("_on_ad_closed")
+	watch_ad.disabled = true
+	get_coin.disabled = true
+	init_coin()
+
+func _on_GetCoin_pressed():
+	CoinManager.add_coin(1)
+	watch_ad.disabled = true
+	get_coin.disabled = true
+	init_coin()
+	
+func _on_WatchAd_pressed():
+	AdsManager.show_ad()
